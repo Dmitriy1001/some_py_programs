27cf9376c7aad1a9757ea33f70895fcbadeda2c3
Dmitriy1001 <dumatkachuk@gmail.com>
2021-05-26 15:39:55 +0300

    first

---
+++ are_texts_match/are_texts_match.py
@@ -0,0 +1,33 @@
+from difflib import SequenceMatcher
+import numpy as np
+import itertools
+
+def match_texts(text1, text2):
+    '''Returns true if the two texts match 70 percent or more'''
+    similarity = lambda x: np.mean([SequenceMatcher(None, a,b).ratio() for a,b in itertools.combinations(x, 2)])
+    text1, text2 = ' '.join(sorted(text1.lower().split())), ' '.join(sorted(text2.lower().split()))
+    return similarity([text1, text2]) >= 0.7
+
+
+def read_file(file):
+	with open(file, encoding='utf-8') as f:
+		return f.read().strip()
+
+
+def main():
+	input_or_path = input(
+	'Do you want to input strings or specify the path to text files? 1-input, 2-path: '
+	)
+	if input_or_path == '1':
+		text1 = input('Input text 1:')
+		text2 = input('Input text 2:')
+	else:
+		text1 = read_file(input('Path to file 1:')) 
+		text2 = read_file(input('Path to file 2:'))
+	print('The texts math' if match_texts(text1, text2) else \
+		  'The texts do not math')
+
+
+if __name__ == '__main__':
+	main()
+
