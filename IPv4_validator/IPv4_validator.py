de062a32c3c2cb5160dd7b1d4d72e9f7ff345a89
Dmitriy1001 <dumatkachuk@gmail.com>
2021-05-26 16:19:39 +0300

    added "ip validator" program

---
+++ IPv4_validator/IPv4_validator.py
@@ -0,0 +1,10 @@
+def ip_validator(ip):
+	'''verifys the validity of IPv4 addresses'''
+    ip = ip.split('.')
+    return all(n.isdigit() for n in ip) and all(0 <= int(n) <= 255 for n in ip) \
+           and len(ip) == 4
+
+
+print(
+	'Valid ip' if ip_validator(input('Input a ip: ')) else 'Not valid ip'
+)
