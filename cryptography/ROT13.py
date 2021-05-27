948ebc8f42e4a18e17b8d70ecff504343024c403
Dmitriy1001 <dumatkachuk@gmail.com>
2021-05-27 08:25:32 +0300

    edit cryptography/ROT13.py

--- cryptography/ROT13.py
+++ cryptography/ROT13.py
@@ -2,7 +2,7 @@ from string import ascii_lowercase as alphabet
 
 
 def rot13(s):
-    '''Encrypts and decrypts the string with ROOT13 cipher'''
+    '''Encrypts and decrypts the string with ROT13 cipher'''
     alph = list(alphabet)
     res, low_upper = [], 0
     for i in s:
@@ -15,5 +15,8 @@ def rot13(s):
             res.append(i)
     return ''.join(res)
 
+
+# rot13("EBG13 rknzcyr.") == "ROT13 example.";
+# rot13("This is my first ROT13 excercise!" == "Guvf vf zl svefg EBG13 rkprepvfr!"
 
 print(rot13(input('input string: ')))     
